663601d439223124fd81a0978b101ce53ca5910d
jeanius <2106494P@student.gla.ac.uk>
2014-02-28 17:11:51 +0000

    working on folder-bookmark connection

--- dragondrop/views.py
+++ dragondrop/views.py
@@ -13,18 +13,20 @@ def index(request):
 def userpage(request, user_page_url):
      context = RequestContext(request)
      
-     user_name = user_page_url.replace('_', ' ')
-
+     user_name = decode_url(user_page_url)
      context_dict = {'user_name': user_name}
-     context_dict['folder_names'] = ['Programming','Cooking','Biology','Chemistry']
 
      try:
           users = User.objects.get(username=user_name)
           context_dict['user'] = users
-
+          folders = Folder.objects.filter(fusername_fk=users)
+          context_dict['folders'] = folders
+          for folder in folders:
+               folder.url = encode_url(folder.foldername)
+          
      except User.DoesNotExist:
           pass
-     
+   
      if request.method == 'POST':
         query = request.POST['query'].strip()
         if query:
@@ -60,4 +62,27 @@ def register(request):
      return render_to_response(
                'register.html', {'registered': registered, 'username': username, 'user_form': user_form}, context)
 
+def folder(request, folder_name_url):
+     context = RequestContext(request)
+
+     folder_name = decode_url(folder_page_url)
+     context_dict = {'folder_name': folder_name}
+
+     try:
+          folders = Folder.objects.filter(foldername = folder_name)
+          context_dict['folders'] = folders
+          bookmarks = Bookmark.objects.filter(fname = folders)
+          context_dict['bookmarks'] = bookmarks
+
+     except Folder.DoesNotExist:
+          pass
+
+     return render_to_response('folder.html', context_dict, context)
+
+     
+def encode_url(str):
+    return str.replace(' ', '_')
+
+def decode_url(str):
+    return str.replace('_', ' ')
 
