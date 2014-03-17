5ecd56fb128b818c867cb7ff08474f0b434389f5
James Trimble <jamestrimble@Jamess-iMac.local>
2014-03-17 22:35:39 +0000

    Switched debug mode on

--- dragondrop_proj/settings.py
+++ dragondrop_proj/settings.py
@@ -7,7 +7,7 @@ TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 STATIC_PATH = os.path.join(PROJECT_PATH,'static')
 DATABASE_PATH = os.path.join(PROJECT_PATH, 'dragondrop.db')
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 LOGIN_URL = 'index'
